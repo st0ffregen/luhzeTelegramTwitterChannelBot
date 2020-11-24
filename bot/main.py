860d076b554134fcf0777db1afd18a27378e4931
st0ffregen <stoffregen@mailbox.org>
2020-11-24 13:24:26 +0100

    feat: split tweet in essential parts, closes #7

--- bot/main.py
+++ bot/main.py
@@ -21,10 +21,11 @@ def OAuth():
 
 
 def getLastTweet(api):
-    return api.user_timeline(id="@luhze_leipzig", count=10, tweet_mode='extended')#api.me(), count=10)[i])
+    return api.user_timeline(id="@luhze_leipzig", count=30, tweet_mode='extended')#api.me(), count=10)[i])
 
 
 def getValidateTweet(tweetArray):
+    print("validate tweets")
     validTweets = []
     for tweet in tweetArray:
         if (datetime.now() - tweet.created_at).seconds < int(os.environ['INTERVAL_SECONDS']) and \
@@ -36,11 +37,45 @@ def getValidateTweet(tweetArray):
     return validTweets
 
 
-def fetchNewTweets():
+def resolveLinkShortener(tweetArray):
+    return 0
+
+
+def splitTweetInParts(tweetArray, bot, telegramAdminChatId):
+    print("split tweet in parts")
+    splitTweetArray = []
+    for tweet in tweetArray:
+        teaser = tweet.full_text.split("\n")[0].strip()
+        pictureLink = tweet.entities['media'][0]['media_url_https'].strip()
+        linkToArticle = tweet.entities['urls'][0]['expanded_url'].strip()
+        # get credits if present
+        splitOnCameraSign = tweet.full_text.split(u"\U0001F4F8")
+        if len(splitOnCameraSign) > 1:
+            try:
+                pictureCredits = splitOnCameraSign[1].split(" ")
+                photographerName = ""
+                for i in range(0, len(pictureCredits)-1):
+                    photographerName += " " + pictureCredits[i]
+
+            except IndexError as e:
+                print("picture credits present in the wrong format in " + tweet.entities['media'][0]['url'])
+                bot.send_message(chat_id=telegramAdminChatId,
+                                 text="picture credits present in the wrong format in " + tweet.entities['media'][0]['url'])
+        else:
+            photographerName = None
+        splitTweetArray.append({'tweet': tweet, 'teaser': teaser, 'pictureLink': pictureLink,
+                                'linkToArticle': linkToArticle, 'pictureCredits': photographerName})
+
+    return splitTweetArray
+
+
+def fetchNewTweets(bot, telegramAdminChatId):
     oauth = OAuth()
     api = tweepy.API(oauth)
-    validTweets = getValidateTweet(getLastTweet(api))
-
+    lastTweets = getLastTweet(api)
+    validTweets = getValidateTweet(lastTweets)
+    splitTweetArray = splitTweetInParts(validTweets, bot, telegramAdminChatId)
+  
 
 
 def main():
@@ -48,16 +83,19 @@ def main():
     print("starting bot")
     print("utc time now: " + datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
 
+    telegramAdminChatId = os.environ['TELEGRAM_ADMIN_CHAT_ID']
+
     bot = None
 
     try:
-        fetchNewTweets()
         bot = initTelegramBot()
+        fetchNewTweets(bot, telegramAdminChatId)
+
 
     except tweepy.error.TweepError as e:
         print(f"error while working with tweepy: {e}")
         traceback.print_exc()
-        bot.send_message(chat_id=os.environ['TELEGRAM_ADMIN_CHAT_ID'],
+        bot.send_message(chat_id=telegramAdminChatId,
                          text=f"error while working with tweepy: {e}")
         print("exiting")
         print(sys.exc_info())
@@ -65,7 +103,7 @@ def main():
     except telegram.TelegramError as e:
         print(f"error while working with telegram api: {e}")
         traceback.print_exc()
-        bot.send_message(chat_id=os.environ['TELEGRAM_ADMIN_CHAT_ID'],
+        bot.send_message(chat_id=telegramAdminChatId,
                          text=f"error while working with telegram api: {e}")
         print("exiting")
         print(sys.exc_info())
